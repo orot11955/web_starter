fcae8236b7210e0502fd18fdc8d6dc6181590d0d
orot <orot1195@gmail.com>
2026-06-17 16:33:41 +0900

    was add

---
+++ server/src/main/java/com/project/web_starter_was/WebStarterWasApplication.java
@@ -0,0 +1,13 @@
+package com.project.web_starter_was;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStarterWasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebStarterWasApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/project/web_starter_was/WebStarterWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.web_starter_was;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStarterWasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
